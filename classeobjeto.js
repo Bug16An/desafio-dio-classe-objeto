80c6b9e85f93fb226570bf0a21dc6515c3144c5f
Bug16An <junior.h.ferrante@gmail.com>
2024-01-19 18:50:45 -0300

    2commitclasse-objeto

--- classeobjeto.js
+++ classeobjeto.js
@@ -1,1 +1,25 @@
-console.log("hello Word");
+class dataOfHero{
+        constructor(nameOfHero, ageOfHero, classOfHero, attackOfHero){
+            this.nameOfHero = nameOfHero
+            this.ageOfHero = ageOfHero
+            this.classOfHero = classOfHero
+            this.attackOfHero = attackOfHero
+        }
+    show(){
+        for ()
+        console.log()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
